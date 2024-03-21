f5e9d50cd08d36ddcb91c03bc213c3261d41fd97
Kedwa52 <kedwa52@lsu.edu>
2024-03-20 23:40:35 -0500

    building Sound Effect

--- Sound/SoundEffects/sketch.js
+++ Sound/SoundEffects/sketch.js
@@ -1,7 +1,33 @@
+function preload(){
+  truck = loadImage ('assets/truckerhorn.png')
+}
+
+
+
+
+let saw = new Tone.Synth({
+  oscillator: {
+    type: "sawtooth"
+  },
+  envelope : {
+    attack: 0.01,
+    decay: 0.01,
+    sustain: 0.5,
+    release: 0.01,
+  }
+}).toDestination();
+
+
+function mousePressed() { 
+saw.triggerAttackRelease("c3", 1.5);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400); 
 }
 
 function draw() {
-  background(220);
+  background(truck);
+  text ('press the truck!', 50, 150);
 }
+  
